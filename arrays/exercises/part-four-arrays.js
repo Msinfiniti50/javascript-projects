bf14218e498c433e0f41c97ff411fc42c42ba25c
tunisia <hjones0973@gmail.com>
2024-01-15 17:49:03 -0600

    completed chpter 1-6

--- arrays/exercises/part-four-arrays.js
+++ arrays/exercises/part-four-arrays.js
@@ -3,8 +3,35 @@ let holdCabinet2 = ['orange drink', 'nerf toys', 'camera', 42, 'parsnip'];
 
 //Explore the methods concat, slice, reverse, and sort to determine which ones alter the original array.
 
-//1) Print the result of using concat on the two arrays. Does concat alter the original arrays? Verify this by printing holdCabinet1 after using the method.
+/*let newArray = [];
+newArray = holdCabinet1.concat(holdCabinet2);
+console.log(newArray);
+newArray = holdCabinet2.concat(holdCabinet1);
+console.log(newArray);
+console.log(holdCabinet1);
+console.log(holdCabinet2);
+console.log(holdCabinet1);*/
+//1) Print the result of using concat on the two arrays. Does concat alter the original arrays?"No" Verify this by printing holdCabinet1 after using the method.
+//let arr = [1,2,3];
+//let otherArray = ['M', 'F', 'E',];
+//let newArray = [];
 
-//2) Print a slice of two elements from each array. Does slice alter the original arrays?
+//newArray = arr.concat(otherArray);
+//console.log(newArray);
 
-//3) reverse the first array, and sort the second. What is the difference between these two methods? Do the methods alter the original arrays?
+//newArray = otherArray.concat(arr);
+//console.log(newArray);
+//    console.log(arr.concat(otherArray, arr));
+ //   console.log(arr);
+//2) Print a slice of two elements from each array. Does slice alter the original arrays? yes
+//holdCabinet1.splice(0,2);
+//console.log(holdCabinet1);
+//holdCabinet2.splice(0,2);
+//console.log(holdCabinet2);
+//3) reverse the first array, and sort the second. What is the difference between these two methods? 1 reverses the order and the other alphabetical the other Do the methods alter the original arrays? no
+//holdCabinet1.reverse();
+//console.log(holdCabinet1);
+//holdCabinet2.sort();
+//console.log(holdCabinet2);
+console.log(holdCabinet1);
+console.log(holdCabinet2);
