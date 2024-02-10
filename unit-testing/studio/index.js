00a5bb702e72b276896cb5cf9a1c246b2ac330d5
Msinfiniti50 <hjones0973@gmail.com>
2024-02-09 22:20:12 -0600

    launchcode organization complete

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,50 @@
-
 let launchcode = {
+    organization: "nonprofit",
+    executiveDirector: "Jeff",
+    percentageCoolEmployees: 100,
+    programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
+    launchOutput: function(number) {
+        let conditions = {
+            2: "Launch",
+            3: "Code",
+            5: "Rocks"
+        };
+
+        if (number % 2 === 0 && number % 3 === 0) {
+            return "LaunchCode!";
+        }
+
+        let output = "";
+        for (let divisor in conditions) {
+            if (number % divisor === 0) {
+                output += conditions[divisor] + " ";
+            }
+        }
+        output = output.trim();
+
+        if (output === "") {
+            return "Rutabagas! That doesn't work.";
+        } else if (output === "Launch Rocks") {
+            return output + "! (CRASH!!!)";
+        } else {
+            return output + "!";
+        }
+    }
+}    
+
+
+
   
-}
+//let launchCode = function(number){
+//    if (number % 2 === 0 && number %3 === 0){
+//        return "Launchcode!";
+    
+  
 
 module.exports = launchcode;
+organization: "nonprofit";
+executiveDirector: "Jeff";
+percentageCoolEmployees: 100;
+programsOffered: ["Web Development", "Data Analysis", "Liftoff"];
+launchOutput: ["Launch!", "Code!", "Rocks!", "LaunchCode!", "Code Rocks!", "Launch Rocks!", "LaunchCode Rocks!", "Rutabagas! That doesn't work."];
 
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -1,8 +1,65 @@
 // launchcode.test.js code:
 const launchcode = require('../index.js');
-
 describe("Testing launchcode", function(){
 
-  // Write your unit tests here!
-  
+describe("Testing launchcode", function(){
+  test("launchcode: organization", function(){
+    expect(launchcode.organization).toBe("nonprofit");
+  });
+  test("launchcode: executiveDirector", function(){
+    expect(launchcode.executiveDirector).toBe("Jeff");
+  });
+  test("launchcode: percentageCoolEmployees", function(){
+    expect(launchcode.percentageCoolEmployees).toBe(100);
+  });
+  test("launchcode: programsOffered", function(){
+    expect(launchcode.programsOffered).toContain("Web Development");
+    expect(launchcode.programsOffered).toContain("Data Analysis");
+    expect(launchcode.programsOffered).toContain("Liftoff");
+    expect(launchcode.programsOffered.length).toBe(3);
+  });
 });
+  describe("Testing launchOutput", function(){
+  test("launchOutput: Launch",  function(){
+    expect(launchcode.launchOutput(2)).toBe("Launch!");
+    });
+  });
+  describe("Testing launchOutput", function(){
+    test("launchOutput: Code!",  function(){
+      expect(launchcode.launchOutput(3)).toBe("Code!");
+      });
+    });
+    describe("Testing launchOutput", function(){
+      test("launchOutput: Rocks!",  function(){
+        expect(launchcode.launchOutput(5)).toBe("Rocks!");
+        });
+      });
+      describe("Testing launchOutput", function(){
+        test("launchOutput: LaunchCode!",  function(){
+          expect(launchcode.launchOutput(6)).toBe("LaunchCode!");
+          });
+        });
+        
+          describe("Testing launchOutput", function(){
+            test("launchOutput: Launch Rocks! (CRASH!!!)",  function(){
+              expect(launchcode.launchOutput(10)).toBe("Launch Rocks! (CRASH!!!)");
+              });
+            });
+          });
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+  // Write your unit tests here!//
