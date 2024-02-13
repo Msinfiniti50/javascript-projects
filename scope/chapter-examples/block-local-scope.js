47c7dfed36538377a13bf40819531e0fcaebbb24
Msinfiniti50 <hjones0973@gmail.com>
2024-02-12 18:57:21 -0600

    divided completed

--- scope/chapter-examples/block-local-scope.js
+++ scope/chapter-examples/block-local-scope.js
@@ -1,6 +1,10 @@
-function myFunction() {
-    let i = 10;
-    return 10 + i;
-}
+let i = 10;
+//function myFunction() {
+//    let i = 10;
+//    return 10 + i;
+//}
 
-console.log(i);
+//console.log(i);
+//let x = null;
+//console.log(x);
+
