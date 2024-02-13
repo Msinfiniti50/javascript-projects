118c5838bdab25c8be43e2f162efb34e60017721
Msinfiniti50 <hjones0973@gmail.com>
2024-02-13 15:18:44 -0600

    class exercise completed

--- classes/chapter-examples/ClassExamples02.js
+++ classes/chapter-examples/ClassExamples02.js
@@ -2,16 +2,40 @@
 
 // Next, set default values for 1 or more of the parameters in constructor.
 
-class Astronaut {
-   constructor(name, age, mass){
-     this.name = name;
-     this.age = age;
-     this.mass = mass;
-   }
-}
+// class Astronaut {
+//    constructor(name, age = 50, mass = 159){
+//      this.name = name;
+//      this.age = age;
+//      this.mass = mass;
+//    }
+// }
+// class Car {
+//   constructor(make, model, year, color, mpg){
+//      this.make = make;
+//      this.model = model;
+//      this.year = year;
+//      this.color = color;
+//      this.mpg = mpg;
+//   }
+// }
+//let tortoise = new Astronaut('Speedy', 120);
 
-let tortoise = new Astronaut('Speedy', 120);
-
-console.log(tortoise.name, tortoise.age, tortoise.mass);
-
+//console.log(tortoise.name, tortoise.age, tortoise.mass);
+//let myCar = new Car('Chevy', 'Astro', 1985, 'gray', 20);
+// let myCar = new Car('Tesla', 'Model S', 2019)
+// console.log(myCar)
+// console.log(typeof myCar .year)
 // What happens if we call Astronaut and pass in MORE than 3 arguments? TRY IT!
+
+class plant{
+  constructor(type, height) {
+    this.type = 'hydrandria';
+    this.height = 10;
+  }
+  grow() {
+   this.height = this.height + 1;
+  }
+}
+let myPlant = new plant('hydrandria', 10);
+myPlant.grow();
+console.log(myPlant.height);
--- classes/exercises/ClassExercises.js
+++ classes/exercises/ClassExercises.js
@@ -1,10 +1,64 @@
 // Define your Book class here:
-
+class Book {    
+    constructor (title, author, copyRightDate, ISBN, numberOfPages, timesCheckedOut, bookHadBeenDiscarded) {
+        this.title = title;
+        this.author = author;
+        this.copyRightDate = copyRightDate;
+        this.isbn = ISBN;
+        this.pages = numberOfPages;
+        this.timesCheckedOut = timesCheckedOut;
+        this.bookHadBeenDiscarded = bookHadBeenDiscarded;
+       
+    }
+    checkOut(uses=1) {
+        this.timesCheckedOut += uses;
+    }
+}
 
 // Define your Manual and Novel classes here:
-
+class Manual extends Book{    
+    constructor (title, author, copyRightDate, ISBN, numberOfPages, timesCheckedOut, bookHadBeenDiscarded) {
+        super(title, author, copyRightDate, ISBN, numberOfPages, timesCheckedOut, bookHadBeenDiscarded);
+     }
+     dispose(currentYear) {
+        if (currentYear-this.copyRightDate>5) {
+        this.bookHadBeenDiscarded = 'Yes';    
+        }
+      }
+    }
+class Novel extends Book{    
+    constructor (title, author, copyRightDate, ISBN, numberOfPages, timesCheckedOut, bookHadBeenDiscarded) {
+        super(title, author, copyRightDate, ISBN, numberOfPages, timesCheckedOut, bookHadBeenDiscarded);
+   }
+    dispose(){
+     if (this.timesCheckedOut >= 100) {
+       this.bookHadBeenDiscarded = 'Yes';
+    }
+  }
+}
 
 // Declare the objects for exercises 2 and 3 here:
+let myLibrary = new Manual('Top Secret Shuttle Building Manual', 'Redacted', 2013, '0000000000000', 1147, 1, 'No');
+let myNovel = new Novel('Pride and Prejudice', 'Jane Austen', 1813, '1111111111111', 432, 32, 'No');   
 
 
 // Code exercises 4 & 5 here:
+let oldBooks = [myLibrary, myNovel];
+let currentYear = new Date().getFullYear();
+
+oldBooks.forEach(book => {
+    if (book instanceof Manual) {
+      book.dispose(currentYear);
+    } else if (book instanceof Novel) {
+      book.dispose();
+    }
+  });
+  
+  myLibrary.checkOut(5);
+  myLibrary.dispose(currentYear);
+  
+  myNovel.checkOut(5);
+  myNovel.dispose();
+
+console.log(`${myLibrary.title} is a book by ${myLibrary.author}, Has it been discarded? ${myLibrary.bookHadBeenDiscarded}`); 
+console.log(myNovel.bookHadBeenDiscarded);
