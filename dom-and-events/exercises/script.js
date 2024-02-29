735d39e6a1996b519744327ac88707dcd1236ec0
Msinfiniti50 <hjones0973@gmail.com>
2024-02-29 14:41:02 -0600

    script.js

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,21 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
-}
 
-window.addEventListener("load", init);
+    button.addEventListener('click', function() {
+        paragraph.textContent = 'Houston, we have liftoff!';
+    });
+    missionAbort.addEventListener('mouseover', function(event) {
+        event.target.style.backgroundColor = 'red';
+    });
+    missionAbort.addEventListener('mouseout', function(event) {
+        event.target.style.backgroundColor = '';
+    });
+    missionAbort.addEventListener('click', function() {
+        let response = window.confirm('Are you sure you want to abort the mission?');
+        if (response) {
+            paragraph.textContent = 'Mission aborted. Space shuttle returning home.';
+        }
+    });
+    
+}window.onload = init;
